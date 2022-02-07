be18960be6cbdff55ca0f22dca8361eb69c54ee4
Wraeclastt <98897116+Wraeclastt@users.noreply.github.com>
2022-02-07 10:55:48 -0400

    Initial commit

---
+++ src/Lives.js
@@ -0,0 +1,20 @@
+import { Entity } from "./Entity";
+
+export class Lives extends Entity {
+  constructor() {
+    super();
+    this.lives = 3;
+    this.setX(window.innerWidth / 2);
+    this.setY(window.innerHeight - 50);
+    this.refreshText();
+  }
+
+  removeLife() {
+    this.lives--;
+    this.refreshText();
+  }
+
+  refreshText() {
+    this.el.innerText = new Array(this.lives).fill(`❤`).join("  ");
+  }
+}
